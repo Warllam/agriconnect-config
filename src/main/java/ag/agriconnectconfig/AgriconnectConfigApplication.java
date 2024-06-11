f7342a15932efbc69ae0c17d2d851106d4493db7
PAN <paularthur@navaud.fr>
2024-06-11 16:38:53 +0200

    Fixing Config

--- src/main/java/ag/agriconnectconfig/AgriconnectConfigApplication.java
+++ src/main/java/ag/agriconnectconfig/AgriconnectConfigApplication.java
@@ -2,10 +2,14 @@ package ag.agriconnectconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// Activation serveur de configuration
 @EnableConfigServer
+// Activation enregistrement Annuaire
+@EnableDiscoveryClient
 public class AgriconnectConfigApplication {
 
 	public static void main(String[] args) {
