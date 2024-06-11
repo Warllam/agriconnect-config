e7b236e372f222cd7ea7b0cc4ebe9e490c6ab876
Eliott-Tixier <93209602+Eliott-Tixier@users.noreply.github.com>
2024-06-11 15:21:42 +0200

    update config

---
+++ src/test/java/ag/agriconnectconfig/AgriconnectConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ag.agriconnectconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriconnectConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
